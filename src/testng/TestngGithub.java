0536083fb0d8adfd2e00168c86f6fde8db1d3ae8
Naveen-33 <naveenkumarreddy.thamatam@sithafal.com>
2024-01-04 23:49:04 +0530

    Update TestngGithub.java

--- src/testng/TestngGithub.java
+++ src/testng/TestngGithub.java
@@ -1,14 +1,20 @@
 package testng;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class TestngGithub {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void google() {
+	  
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
