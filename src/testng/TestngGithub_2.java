850d1f240aa1a457911e6641ac8c9f7e8a3474bb
Naveen-33 <naveenkumarreddy.thamatam@sithafal.com>
2024-01-05 00:10:28 +0530

    SeleniumGithub Code is Pushed

---
+++ src/testng/TestngGithub_2.java
@@ -0,0 +1,14 @@
+package testng;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class TestngGithub_2 {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
